d6cd4596026a58fb13f8c438b97758c2f1e25c0e
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-05-01 09:34:58 +0000

    [finish5]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+   get "search" => "searches#search"
   get 'relationships/followings'
   get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -11,8 +12,8 @@ Rails.application.routes.draw do
      resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-  
-  
+
+
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
